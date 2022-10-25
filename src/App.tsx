e29ff1c7fc5c20a84ad9f520703e0f7884c979f2
Kadu <kadu1998@outlook.com>
2022-10-25 18:28:42 -0300

    development: finance summary area

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,47 @@ import { filterListByMonth, getCurrentMonth } from "./helpers/dateFilter";
 import { TableArea } from "./components/TableArea";
 import { InfoArea } from "./components/InfoArea";
 
-
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
-    setFilteredList( filterListByMonth(list, currentMonth))
-  }, [list, currentMonth])
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth]);
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if (Categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+  }, [filteredList]);
 
   const handleMonthChange = (newMonth: string) => {
-    setCurrentMonth(newMonth)
-  }
+    setCurrentMonth(newMonth);
+  };
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        <InfoArea currentMonth={currentMonth}
-        onMonthChange={handleMonthChange}
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
         />
 
         {/* Area de inserção*/}
